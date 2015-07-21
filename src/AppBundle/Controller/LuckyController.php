48d4f8ba63b0ff5e3c46386d4d1ef549907799f1
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2015-07-21 13:13:26 +0300

    modified the code to fix some sensiolabsinsight recomendations

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
